a5c60ba9ca89932b78e10280d692a6f30be69f22
vagrant <vagrant@precise32.(none)>
2016-02-02 08:43:35 +0000

    category bug fix

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,54 +1,54 @@
 class Admin::CategoriesController < Admin::BaseController
-  cache_sweeper :blog_sweeper
+	cache_sweeper :blog_sweeper
 
-  def index; redirect_to :action => 'new' ; end
-  def edit; new_or_edit;  end
+	def index; redirect_to :action => 'new' ; end
+	def edit; new_or_edit;  end
 
-  def new 
-    respond_to do |format|
-      format.html { new_or_edit }
-      format.js { 
-        @category = Category.new
-      }
-    end
-  end
+	def new 
+		respond_to do |format|
+			format.html { new_or_edit }
+			format.js { 
+				@category = Category.new
+			}
+		end
+	end
 
-  def destroy
-    @record = Category.find(params[:id])
-    return(render 'admin/shared/destroy') unless request.post?
+	def destroy
+		@record = Category.find(params[:id])
+		return(render 'admin/shared/destroy') unless request.post?
 
-    @record.destroy
-    redirect_to :action => 'new'
-  end
+		@record.destroy
+		redirect_to :action => 'new'
+	end
 
-  private
+	private
 
-  def new_or_edit
-    @categories = Category.find(:all)
-    @category = Category.find(params[:id])
-    @category.attributes = params[:category]
-    if request.post?
-      respond_to do |format|
-        format.html { save_category }
-        format.js do 
-          @category.save
-          @article = Article.new
-          @article.categories << @category
-          return render(:partial => 'admin/content/categories')
-        end
-      end
-      return
-    end
-    render 'new'
-  end
+	def new_or_edit
+		@categories = Category.find(:all)
+		@category = Category.get_or_build_category(params[:id])
+		@category.attributes = params[:category]
+		if request.post?
+			respond_to do |format|
+				format.html { save_category }
+				format.js do 
+					@category.save
+					@article = Article.new
+					@article.categories << @category
+					return render(:partial => 'admin/content/categories')
+				end
+			end
+			return
+		end
+		render 'new'
+	end
 
-  def save_category
-    if @category.save!
-      flash[:notice] = _('Category was successfully saved.')
-    else
-      flash[:error] = _('Category could not be saved.')
-    end
-    redirect_to :action => 'new'
-  end
+	def save_category
+		if @category.save!
+			flash[:notice] = _('Category was successfully saved.')
+		else
+			flash[:error] = _('Category could not be saved.')
+		end
+		redirect_to :action => 'new'
+	end
 
 end
